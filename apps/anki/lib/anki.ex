30262f6405a910ae371c53efbd1877839b0749f1
shouston3 <shouston3@me.com>
2017-11-15 20:57:25 +0700

    fix tests and typos so now Anki.request module works as expected

--- apps/anki/lib/anki.ex
+++ apps/anki/lib/anki.ex
@@ -6,27 +6,27 @@ defmodule Anki do
 
   defp kill_node(), do: System.cmd "pkill", ["node"]
 
-  defp node_result(pid) do
+  defp node_result(pid, endpoint) when is_binary endpoint do
     receive do
       {^pid, :data, :out, log} ->
         IO.puts "Node log => #{log}"
         cond do
           String.contains? log, "running" ->
-            HTTPoison.get! @root_url <> "/collection"
+            HTTPoison.get! @root_url <> endpoint
           true ->
-            node_result(pid)
+            node_result pid, endpoint
         end
     end
   end
 
-  def collection do
+  def request!(endpoint) when is_binary endpoint do
     kill_node()
     System.put_env "NODE_ENV", "#{Mix.env}"
     cmd = "node node_app/src/index.js"
     opts = [out: {:send, self()}]
     %Porcelain.Process{pid: pid} = Porcelain.spawn_shell cmd, opts
 
-    result = node_result(pid)
+    result = node_result pid, endpoint
 
     {"", 0} = kill_node()
 
--- apps/anki/test/anki_test.exs
+++ apps/anki/test/anki_test.exs
@@ -1,11 +1,31 @@
 defmodule AnkiTest do
   use ExUnit.Case, async: false
 
+  require Poison
+
   test "/collection" do
-    assert Anki.collection == "hi"
+    actual = "/collection"
+    |> Anki.request!
+    |> Poison.decode!
+
+    expected = "node_app/test/models.json"
+    |> File.read!
+    |> Poison.decode!
+    |> Map.take(Map.keys actual)
+
+    assert actual == expected
   end
 
-  # test "/notes" do
-  #   assert Anki.notes == "hello"
-  # end
+  test "/notes" do
+    actual = "/notes"
+    |> Anki.request!
+    |> Poison.decode!
+
+    expected = "node_app/test/models.json"
+    |> File.read!
+    |> Poison.decode!
+    |> Map.fetch!("formattedNotes")
+
+    assert actual == expected
+  end
 end
