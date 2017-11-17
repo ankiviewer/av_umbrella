abe74e6fae2a10bbc80fd29c0d135305d160121c
shouston3 <shouston3@me.com>
2017-11-17 12:49:36 +0700

    add notes migration

---
+++ apps/anki/priv/repo/migrations/20171117053811_notes.exs
@@ -0,0 +1,17 @@
+defmodule Anki.Repo.Migrations.Notes do
+  use Ecto.Migration
+
+  def change do
+    create table(:notes) do
+      add :model, :string
+      add :deck, :string
+      add :tags, {:array, :string}
+      add :mod, :date
+      add :one, :string
+      add :two, :string
+      add :rules_status, {:array, :string}
+
+      timestamps()
+    end
+  end
+end
