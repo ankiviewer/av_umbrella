5069a03ae370a94b0aaf346206f52e785f6d699e
shouston3 <shouston3@me.com>
2017-12-18 23:12:43 +0700

    pulling through last upated info and padding this text a little

--- apps/anki_web/lib/anki_web/controllers/home_controller.ex
+++ apps/anki_web/lib/anki_web/controllers/home_controller.ex
@@ -1,9 +1,19 @@
 defmodule AnkiWeb.HomeController do
   use AnkiWeb, :controller
 
-  alias AnkiWeb.LayoutView
+  alias Anki.{Repo, Collection}
+
+  @months ~w(jan feb mar apr may jun jul aug sep oct nov dec)
 
   def index(conn, _params) do
-    render conn, "index.html"
+    naive = Collection |> Repo.one |> Map.fetch!(:updated_at)
+
+    %{day: day, hour: hour, minute: minute, month: month, year: year} = naive
+    month = @months |> Enum.at(month) |> String.capitalize
+    year = year - 2000
+
+    updated_at = "#{day} #{month} #{year} at #{hour}:#{minute}"
+    
+    render conn, "index.html", updated_at: updated_at
   end
 end
