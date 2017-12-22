d4b656c66fc199a1443a5cd579239c34a7bf6f3c
shouston3 <shouston3@me.com>
2017-12-22 08:52:58 +0000

    requiring an ANKI_SQLITE_PATH enviro var instead of hardcoding it

--- apps/anki/node_app/src/config.js
+++ apps/anki/node_app/src/config.js
@@ -7,19 +7,15 @@ const path = require('path');
 const server = new Hapi.Server();
 server.connection({ port });
 
+const sqlitePath = process.env.ANKI_SQLITE_PATH;
+
+if (!sqlitePath) {
+  throw 'Missing $ANKI_SQLITE_PATH environment variable';
+}
+
 const db = (process.env.NODE_ENV || '').toUpperCase() === 'TEST'
   ? new sqlite3.Database(':memory:')
-  : new sqlite3.Database(
-    path.join(
-      process.env.HOME,
-      'Library',
-      'Application\ Support',
-      'Anki2',
-      'sam',
-      'collection.anki2'
-    ),
-    sqlite3.READ_ONLY
-  );
+  : new sqlite3.Database(sqlitePath, sqlite3.READ_ONLY);
 
 server.route(routes(db));
 
