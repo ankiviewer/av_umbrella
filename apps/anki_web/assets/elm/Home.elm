583ab72473cd4c707b3bf85bdbc66e934f71a311
shouston3 <shouston3@me.com>
2017-12-24 09:20:42 +0000

    initial home page rendered with elm

--- apps/anki_web/assets/elm/Home.elm
+++ apps/anki_web/assets/elm/Home.elm
@@ -1,8 +1,43 @@
 module Home exposing (..)
 
+import Html exposing (Html, h3, h4, div, button, img, text)
+import Html.Attributes exposing (id, class, src)
 
-import Html exposing (text)
+main
+  = Html.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
 
+type alias Model =
+  { updatedAt : String
+  , syncing : Bool
+  }
 
-main =
-    text "Hello Home"
+init : ( Model, Cmd Msg )
+init = ( initialModel, Cmd.none )
+
+initialModel : Model
+initialModel =
+  { updatedAt = "..."
+  , syncing = False
+  }
+
+type Msg
+  = M
+
+view : Model -> Html Msg
+view model = div []
+  [ h3 [] [ text "Last Updated:" ]
+  , h4 [] [ text "8:00 24 Dec 2017" ]
+  , button [ id "sync_button", class "dib pa2 bg-light-grey br3" ] [ img [ src "/images/reload.svg" ] [] ]
+  , div [] [ text "spinner" ]
+  ]
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model = ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
--- apps/anki_web/assets/js/home.js
+++ apps/anki_web/assets/js/home.js
@@ -1,1 +1,4 @@
 import './home/sync.js';
+
+var node = document.querySelector('#home');
+var home_app = Elm.Home.embed(node);
