9e30ddb106154a647513315699f9dc0a32dc81e1
shouston3 <shouston3@me.com>
2017-12-27 12:56:24 +0000

    increase connection timeoutnightwatch selenium server

--- apps/anki_web/assets/nightwatch.conf.js
+++ apps/anki_web/assets/nightwatch.conf.js
@@ -25,7 +25,7 @@ const config = {
       selenium_host: 'localhost',
       silent: true,
       globals: {
-        waitForConditionTimeout: 10000
+        waitForConditionTimeout: 20000
       }
     },
 
